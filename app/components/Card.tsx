95defb87c9e747e8fcd7c212f2830aa88f6cce40
Bassam Assaf <bassamassaf@Akrams-MacBook-Pro.local>
2023-11-11 05:47:39 +0400

    changed button added two gammes fixed and is now responsive

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,20 +1,55 @@
 import React from "react";
 import Link from "next/link";
 
-const Card = ({ href, title, text }: {href: string; title: string; text?: string}) => {
+const Card = ({
+  href,
+  title,
+  text,
+}: {
+  href: string;
+  title: string;
+  text?: string;
+}) => {
   return (
-    <div className="mr-4 mb-8">
-      <div className="opacity-60 max-w-sm max-h-xs p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="mr-4 mb-8 relative">
+      <div className="opacity-60 min-w-xs max-w-m max-h-xs p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <Link href={href}>
-          <h5 className="opacity-100 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+          <h5 className="opacity-100 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
         </Link>
-        <p className="pt-2 pb-2 font-normal text-gray-700 dark:text-gray-400">{text}</p>
-        <Link href={href} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Go!
-          <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-          </svg>
-        </Link>
+        <p className="pt-2 pb-6 font-normal text-gray-700 dark:text-gray-400 ">
+          {text}
+        </p>
+        <div className="group relative">
+          <div className="absolute inset-0.5 bg-black rounded-lg blur opacity-75 z-0"></div>
+          <div className="absolute inset-1.5 z-10 flex items-center justify-center">
+            <span className="text-white"></span>
+            <Link
+              href={href}
+              className="inline-flex items-center px-12 pb-2 py-2 ml-2 text-sm font-medium text-white bg-gradient-to-r from-sky-600 to-purple-600 rounded-lg opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"
+            >
+            Go
+              <svg
+              
+                className="w-3.5 h-3.5 ml-2"
+                
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ import "./page.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <main className=" flex min-h-screen flex-col items-center justify-between p-6 sm:p-8 md:p-10 lg:p-12 xl:p-16 container">
+    <div className="">
+      
+      <main className="flex flex-col items-center p-6 sm:p-8 md:p-10 lg:p-12 xl:p-16 2xl:p-20 max-w-screen-2xl">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extralight pb-4 mt-[-40px] pt-5">
           Domaindle
         </h1>
@@ -27,13 +28,13 @@ export default function Home() {
           />
           <Card
             href="https://globle-game.com/"
-            text="Your goal is to guess the country. After each guess there will be a color indicating how close you are."
+            text="Your goal is to guess the country. After each guess, there will be a color indicating how close you are."
             title="Globle"
           />
           <Gradient />
           <Card
             href="https://likewisetv.com/arcade/moviedle"
-            text="A fast clip of a movie, after each try the clip gets longer, you have to guess the movie within 5 tries."
+            text="A fast clip of a movie, after each try, the clip gets longer, and you have to guess the movie within 5 tries."
             title="Moviedle"
           />
         </div>
@@ -51,9 +52,24 @@ export default function Home() {
           />
           <Card
             href="https://contexto.me/"
-            text="Find the secret word. You have unlimited guesses. The words were sorted by AI for word similarity"
+            text="Find the secret word. You have unlimited guesses. The words were sorted by AI for word similarity."
             title="Contexto"
           />
+        </div>
+
+        <div className="flex flex-col sm:flex-row gap-4 px-4">
+          <Gradient />
+          <Card
+            href="https://wafflegame.net/daily"
+            text="Rearrange the letters into the correct word horizontally and vertically to match via dragging."
+            title="Waffle"
+          />
+          <Card
+            href="https://www.merriam-webster.com/games/quordle/#/"
+            text="Guess four 5-letter words in 9 tries. Tiles change color after each guess to show correct letters."
+            title="Quordle"
+          />
+          <Gradient />
         </div>
       </main>
     </div>
