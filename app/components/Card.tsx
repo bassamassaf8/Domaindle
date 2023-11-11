f1ff4b3da5a4e54b6050ea28406a4176d045bf41
Bassam Assaf <bassamassaf@Akrams-MacBook-Pro.local>
2023-11-11 21:34:01 +0400

    made responsive all boxes same size, added game

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -11,43 +11,42 @@ const Card = ({
   text?: string;
 }) => {
   return (
-    <div className="mr-4 mb-8 relative">
-      <div className="opacity-60 min-w-xs max-w-m max-h-xs p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <Link href={href}>
-          <h5 className="opacity-100 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {title}
-          </h5>
-        </Link>
-        <p className="pt-2 pb-6 font-normal text-gray-700 dark:text-gray-400 ">
-          {text}
-        </p>
-        <div className="group relative">
-          <div className="absolute inset-0.5 bg-black rounded-lg blur opacity-75 z-0"></div>
-          <div className="absolute inset-1.5 z-10 flex items-center justify-center">
-            <span className="text-white"></span>
-            <Link
-              href={href}
-              className="inline-flex items-center px-12 pb-2 py-2 ml-2 text-sm font-medium text-white bg-gradient-to-r from-sky-600 to-purple-600 rounded-lg opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"
-            >
-            Go
-              <svg
-              
-                className="w-3.5 h-3.5 ml-2"
-                
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+    <div className="min-w-md max-w-md min-h-md max-h-md">
+      <div className="mr-4 ml-4 mb-8 relative max-h-md min-h-md max-h-md">
+        <div className="opacity-80 min-w-xs max-w-m max-h-xs p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
+          <Link href={href}>
+            <h5 className="opacity-100 text-2xl font-bold tracking-tight text-gray-900 dark:text-white pb-2">
+              {title}
+            </h5>
+          </Link>
+          <p className="pt-2 pb-10 font-normal text-gray-700 dark:text-gray-400 min-w-md max-w-md min-h-md max-h-md">
+            {text}
+          </p>
+          <div className="group relative">
+            <div className="absolute inset-0.5 z-10 flex items-center justify-center pb-8">
+              <span className="text-white"></span>
+              <Link
+                href={href}
+                className="inline-flex items-center px-16 pb-2 py-2 ml-2 mr-2 text-sm font-medium text-white bg-gradient-to-r from-sky-600 to-purple-600 rounded-lg opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"
               >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </Link>
+                Go!!
+                <svg
+                  className="w-3.5 h-3.5 ml-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"  
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import "./page.css";
 export default function Home() {
   return (
     <div className="">
-      
       <main className="flex flex-col items-center p-6 sm:p-8 md:p-10 lg:p-12 xl:p-16 2xl:p-20 max-w-screen-2xl">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extralight pb-4 mt-[-40px] pt-5">
           Domaindle
@@ -20,7 +19,6 @@ export default function Home() {
         </h2>
 
         <div className="flex flex-col sm:flex-row gap-4 px-4">
-          <Gradient />
           <Card
             href="https://www.nytimes.com/games/wordle/index.html"
             text="Wordle is a word puzzle game where players guess a hidden five-letter word within six attempts."
@@ -31,7 +29,7 @@ export default function Home() {
             text="Your goal is to guess the country. After each guess, there will be a color indicating how close you are."
             title="Globle"
           />
-          <Gradient />
+
           <Card
             href="https://likewisetv.com/arcade/moviedle"
             text="A fast clip of a movie, after each try, the clip gets longer, and you have to guess the movie within 5 tries."
@@ -39,17 +37,18 @@ export default function Home() {
           />
         </div>
 
-        <div className="flex flex-col sm:flex-row gap-4 px-4">
-          <Card
-            href="https://polygonle.net/"
-            text="Using different shapes that correspond to different letters, use patterns to help figure out the word."
-            title="Polygonle"
-          />
+        <div className="flex flex-col sm:flex-row gap-4 px-4 max-w-s">
           <Card
             href="https://www.mathler.com/"
-            text="Mathler requires you to use math operators to create an equation that equals the given answer."
+            text="Mathler requires you to use math symbols to create an equation that equals the given answer."
             title="Mathler"
           />
+          <Card
+            href="https://heardlewordle.io/"
+            text="Listen to an intro of a song, then find the correct song in the list. Guess in as little tries as possible."
+            title="Heardle"
+          />
+
           <Card
             href="https://contexto.me/"
             text="Find the secret word. You have unlimited guesses. The words were sorted by AI for word similarity."
@@ -58,7 +57,6 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col sm:flex-row gap-4 px-4">
-          <Gradient />
           <Card
             href="https://wafflegame.net/daily"
             text="Rearrange the letters into the correct word horizontally and vertically to match via dragging."
@@ -69,7 +67,11 @@ export default function Home() {
             text="Guess four 5-letter words in 9 tries. Tiles change color after each guess to show correct letters."
             title="Quordle"
           />
-          <Gradient />
+          <Card
+            href="https://polygonle.net/"
+            text="Using different shapes that correspond to different letters, use patterns to help figure out the word."
+            title="Polygonle"
+          />
         </div>
       </main>
     </div>
