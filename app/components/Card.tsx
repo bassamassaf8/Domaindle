cdc907f198e5e6ba6dc5657528aee7f7ef70da8e
Bassam Assaf <bassamassaf@Akrams-MacBook-Pro.local>
2023-11-13 11:09:36 +0400

    dan help changed color

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -14,18 +14,19 @@ const Card = ({
     <div className="min-w-md max-w-md min-h-md max-h-md">
       <div className="mr-4 ml-4 mb-8 relative max-h-md min-h-md max-h-md">
         <div className="opacity-90 min-w-xs max-w-md min-h-md p-7 border border-gray-700  rounded-2xl shadow bg-slate-950 mb-4">
-          <Link href={href}>
+          <Link target="_blank" href={href}>
             <h5 className="opacity-100 text-2xl font-bold tracking-tight text-white pb-2">
               {title}
             </h5>
           </Link>
-          <p className="pt-2 pb-8 font-normal text-gray-700 dark:text-gray-400 min-w-md max-w-md min-h-md max-h-md">
+          <p className="pt-2 pb-8 font-normal text-gray-400 min-w-md max-w-md min-h-md max-h-md">
             {text}
           </p>
           <div className="group relative">
             <div className="absolute inset-0.5 z-10 flex items-center justify-center pb-8">
               <span className="text-white"></span>
               <Link
+              target="_blank"
                 href={href}
                 className="inline-flex items-center px-16 pb-2 py-2 ml-2 mr-2 text-sm font-medium text-white bg-gradient-to-r from-blue-500  to-purple-600 rounded-lg opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"
               >
